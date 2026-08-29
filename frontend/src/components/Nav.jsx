7dad3be41d827eb17c638c9044e24020551d903b
CodeExplorerRwanda <etiene3008@gmail.com>
2026-08-29 09:58:29 +0200

    Creating Nav bar of website

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,8 +1,26 @@
 import {FaHome, FaInfoCircle, FaServicestack, FaBriefcase,FaNewspaper, FaEnvelope } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+import logo from "../assets/logo.png";
 
 const Nav = () => {
     
-    return (
+  return (
+      <div>
+          <div>
+              <div>
+                  <img src={logo}  alt="Logo" />
+              </div>
 
+              <div>
+                   <Link>Home</Link>
+                   <Link>About</Link>
+                   <Link>Services</Link>
+                   <Link>Blog</Link>
+                   <Link>Portifolio</Link>
+                   <Link>Contact</Link>
+              </div>
+          </div>
+     </div>
     )
 }
