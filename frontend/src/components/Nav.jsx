0f007fdcb9858b84a418a3411cff8ea0dc1faa4f
CodeExplorerRwanda <etiene3008@gmail.com>
2026-08-29 10:12:18 +0200

    Creating Nav bar of website

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -7,15 +7,15 @@ const Nav = () => {
     
   return (
       <div>
-          <div className="bg-white flex justify-between">
+          <div className="bg-white flex justify-between max-w-7xl mx-auto">
               <div className="p-3 flex">
                   <img src={logo}  alt="Logo" className="w-15 h-15 rounded-full"/>
-                  <h1 className="mt-5 text-2xl font-bold ms-3"><span className="text-blue-500">Code</span> <span className="text-yellow-500">Explorer</span> <span className="text-green-500">Rwanda</span></h1>
+                  <h1 className="mt-5 text-2xl font-bold ms-3"><span className="text-blue-600">Code</span> <span className="text-yellow-600">Explorer</span> <span className="text-green-600">Rwanda</span></h1>
               </div>
 
-              <div className="flex space-x-4">
-                   <Link><FaHome /> Home</Link>
-                   <Link><FaInfoCircle /> About</Link>
+              <div className="flex space-x-4 p-3 mt-3">
+                   <Link className="inline-flex text-gray-500 text-lg"><FaHome className="me-3 mt-1 text-gray-500" /> <span className="font-bold">Home</span></Link>
+                   <Link className="inline-flex text-gray-500 text-lg"><FaInfoCircle /> About</Link>
                    <Link><FaServicestack /> Services</Link>
                    <Link><FaBriefcase /> Blog</Link>
                    <Link><FaNewspaper /> Portifolio</Link>
